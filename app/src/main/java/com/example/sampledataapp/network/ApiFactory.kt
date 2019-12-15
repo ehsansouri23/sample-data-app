d024f5178705a0a337f9a9e25120dbcac3913f60
Ehsan <ehsansouri23@gmail.com>
2019-12-15 22:56:34 +0330

    add auth header

--- app/src/main/java/com/example/sampledataapp/network/ApiFactory.kt
+++ app/src/main/java/com/example/sampledataapp/network/ApiFactory.kt
@@ -1,14 +1,28 @@
 package com.example.sampledataapp.network
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiFactory {
     const val BASE_URL = "http://stage.achareh.ir/api/karfarmas"
 
+    private val client = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            var request = chain.request()
+            request = request.newBuilder()
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Username", "09822222222")
+                .addHeader("Password", "sana1234")
+                .build()
+            chain.proceed(request)
+        }
+        .build()
+
     private fun retrofit(): Retrofit = Retrofit.Builder()
         .baseUrl("$BASE_URL/")
         .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
         .build()
 
     val api: Api = retrofit().create(Api::class.java)
