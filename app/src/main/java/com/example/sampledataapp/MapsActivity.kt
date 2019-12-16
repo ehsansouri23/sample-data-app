f569a03d23979f36f200e427e63e817ccd566f22
Ehsan <ehsansouri23@gmail.com>
2019-12-16 23:37:12 +0330

    add select location activity

--- app/src/main/java/com/example/sampledataapp/MapsActivity.kt
+++ app/src/main/java/com/example/sampledataapp/MapsActivity.kt
@@ -1,13 +1,24 @@
 package com.example.sampledataapp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Point
 import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.maps.CameraUpdateFactory
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.sampledataapp.util.toast
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
+
+
+const val LOCATION_PERMISSION = 110
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -16,27 +27,70 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        if (!permissionGranted())
+            requestPermission()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    private fun permissionGranted() = ContextCompat.checkSelfPermission(
+        applicationContext,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this@MapsActivity,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            LOCATION_PERMISSION
+        )
+    }
+
+    private fun configPin() {
+        val pin = ImageView(this)
+        pin.setImageResource(R.drawable.ic_pin_drop_black_24dp)
+        val params = FrameLayout.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            Gravity.CENTER
+        )
+        val density = resources.displayMetrics.density
+        params.bottomMargin = (12 * density).toInt()
+        pin.layoutParams = params
+        addContentView(pin, params)
+        selectAddress.setOnClickListener {
+            val position = mMap.projection.fromScreenLocation(
+                Point((pin.left + pin.width / 2), pin.bottom)
+            )
+
+        }
+    }
+
+    private fun configMap() {
+        mMap.apply {
+            isMyLocationEnabled = true
+        }
+    }
+
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        if (permissionGranted())
+            configMap()
+    }
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == LOCATION_PERMISSION) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                configMap()
+            } else
+                toast(getString(R.string.must_accept_address_permission))
+        }
+
     }
 }
